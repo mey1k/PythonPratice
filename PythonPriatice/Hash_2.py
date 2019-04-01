f8af24d044efbbbb82a6f1f53227d26dab2e1bb9
mjkim <mey12k@daum.net>
2019-04-01 22:19:17 +0900

    Hash_2 complete

---
+++ PythonPriatice/Hash_2.py
@@ -0,0 +1,55 @@
+"""
+#특정 문자열이 특정 문자열의 접두사가 된다면 false
+#아니면 true
+#
+"""
+
+import re
+
+def solution(phone_book):
+
+    answer = True
+
+    phone_book = sorted(phone_book)
+
+    for i in range(len(phone_book)):
+        p = re.compile(phone_book[i])
+        for j in range(len(phone_book)):
+         result = p.findall(phone_book[j])
+         if result:
+             if result[0] != phone_book[j]:
+                if result[0][:1] != phone_book[j][:1]:
+                    break
+                else:
+                    answer = False
+                    break
+         else:
+             answer = True
+        if not answer:
+            break
+
+    return print(answer)
+
+
+solution(["12232332", "12", "222222"])
+
+
+#-2
+#import re
+#def solution(phoneBook):
+
+#    for b in phoneBook:
+#        p = re.compile("^"+b)
+#        for b2 in phoneBook:
+#            if b != b2 and p.match(b2):
+#                return False
+#    return True
+
+#-3
+#def solution(phoneBook):
+#    phoneBook = sorted(phoneBook)
+
+#    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+#        if p2.startswith(p1):
+#            return False
+#    return True
