7e054029db726406ed9e5a79f59f96c27781f2e0
mey1k <mey1k@naver.com>
2019-04-05 14:22:15 +0900

    only value

--- PythonPriatice/Hash_3.py
+++ PythonPriatice/Hash_3.py
@@ -4,6 +4,8 @@ combinationsOfValues = []
 answerList = []
 def solution(table):
 
+    answer = 0
+
     for indexTable in range(len(table)):
         if table[indexTable][1] not in hashTable.keys():
             hashTable.setdefault(table[indexTable][1], []).append(table[indexTable][0])
@@ -17,55 +19,40 @@ def solution(table):
     #combinations = [dict(zip(keys, combination)) for combination in itertools.product(*values)]
 
     #1. All combinations of Value of keys
-    for i in  range(1, len(values)+1):
-        combinationsOfValues.append(list(itertools.combinations(values, i))) #[['1개 조합'],['2개 조합'],['3개 조합']- - -]
+    for i in  range(1, len(hashTable.values())+1):
+        #combinationsOfValues.append(list(itertools.combinations(values, i))) #[['1개 조합'],['2개 조합'],['3개 조합']- - -]
         #print(list(itertools.combinations(values, i+1))[0])
 
-    print(list(combinationsOfValues[1]))
+        #answer += len(list(itertools.product(itertools.combinations(values,i))))
+        #print(list(itertools.product(itertools.combinations(values,i))))
+        #for n in itertools.combinations(values,i):
+            #print(n)
+            #print(list(itertools.product(*n)))
+            #answer += len(list(itertools.product(*n)))
+            #for element in itertools.product(*n):
+                #print(element)
+                 #answer += 1
+
+        print(list(values))
+        print(list(itertools.combinations(list(values),3)))
+
+        listlist = (len(list(itertools.product(*n))) for n in itertools.combinations(list(values),i))
+        answer += sum(list(listlist))    
+
+        #print(list(listlist))
+        #answer += (len(list(listlist)))
+    #print(list(combinationsOfValues[1]))
+
     #2. product each combined list
-    for i in range(len(combinationsOfValues)):
-        for j in range(len(list(combinationsOfValues[i]))):
-            for element in itertools.product(*list(combinationsOfValues[i][j])):
-                answerList.append(element)
+    #for i in range(len(combinationsOfValues)):
+    #    for j in range(len(list(combinationsOfValues[i]))):
+    #        for element in itertools.product(*list(combinationsOfValues[i][j])):
+    #            answerList.append(element)
 
-    print(answerList)
-    #arrAnswer = {1:{1:2},2:{1:3}}
-
-    #print(arrAnswer[1].get(3))
-    #for pivotKey in hashTable.keys():
-    #    arrAnswer.append(hashTable.get(pivotKey))
-    #    preValue = hashTable.get(pivotKey)
-    #    pivot += 1
-    #    for nextKeys in list(hashTable.keys())[pivot:]:
-    #       preValue = preValue + " + " + hashTable.get(nextKeys)
-    #       if hashTable.get(pivotKey) != hashTable.get(nextKeys):
-    #           arrAnswer.append(preValue)
+    print(answer)
 
 
-    #for pivotKey in hashTable.keys():
-    #    preItems = {pivotKey:hashTable.get(pivotKey)}
-    #    arrAnswer.update({pivot2:{pivotKey:hashTable.get(pivotKey)}})
-    #    pivot += 1
-    #    for nextKeys in list(hashTable.keys())[pivot:]:
-    #        ssibal = nextKeys
-    #        pivot2 += 1
-    #        preItems.update({nextKeys:hashTable.get(nextKeys)})
-    #        print(arrAnswer[0].get(nextKeys))
-    #        if pivot2 == 1:
-    #             arrAnswer.update({pivot2:preItems.copy()})
-    #        elif arrAnswer[pivot-1].get(ssibal) != arrAnswer[pivot].get(ssibal):
-    #            arrAnswer.update({pivot2:preItems.copy()})
-
-
-        #   preValue = preValue + " + " + hashTable.get(nextKeys)
-        #   if hashTable.get(pivotKey) != hashTable.get(nextKeys):
-        #       arrAnswer.append(preValue)
-        #       return print(arrAnswer)
-                
-            
-        
-
-
+          
 solution([["1","가"],["2","나"],["3","다"],["4","다"],["5","마"]])
 
 
