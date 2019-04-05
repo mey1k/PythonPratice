921662a2bb96f5792136d0b29a83bceae5972051
mey1k <mey1k@naver.com>
2019-04-05 10:46:17 +0900

    Time out exception

--- PythonPriatice/Hash_3.py
+++ PythonPriatice/Hash_3.py
@@ -1,14 +1,34 @@
+import itertools
 hashTable = {}
+combinationsOfValues = []
+answerList = []
 def solution(table):
 
     for indexTable in range(len(table)):
         if table[indexTable][1] not in hashTable.keys():
-            #hashTable.setdefault(table[indexTable][1], table[indexTable][0])
             hashTable.setdefault(table[indexTable][1], []).append(table[indexTable][0])
         else:
             hashTable.setdefault(table[indexTable][1], []).append(table[indexTable][0])
 
-    await
+    keys = hashTable.keys() # '가' '나' '다' '라' 
+    values = (hashTable[key] for key in keys)
+
+    #values = [hashTable[key] for key in keys]  #[['1'],['2'],['3','4'],['5']]
+    #combinations = [dict(zip(keys, combination)) for combination in itertools.product(*values)]
+
+    #1. All combinations of Value of keys
+    for i in  range(1, len(values)+1):
+        combinationsOfValues.append(list(itertools.combinations(values, i))) #[['1개 조합'],['2개 조합'],['3개 조합']- - -]
+        #print(list(itertools.combinations(values, i+1))[0])
+
+    print(list(combinationsOfValues[1]))
+    #2. product each combined list
+    for i in range(len(combinationsOfValues)):
+        for j in range(len(list(combinationsOfValues[i]))):
+            for element in itertools.product(*list(combinationsOfValues[i][j])):
+                answerList.append(element)
+
+    print(answerList)
     #arrAnswer = {1:{1:2},2:{1:3}}
 
     #print(arrAnswer[1].get(3))
