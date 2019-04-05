8d46fc5e839292d2ca3a2c2f07183f309970f90b
mey1k <mey1k@naver.com>
2019-04-05 14:41:55 +0900

    Hash_3 Complete

--- PythonPriatice/Hash_3.py
+++ PythonPriatice/Hash_3.py
@@ -1,53 +1,35 @@
 import itertools
 hashTable = {}
-combinationsOfValues = []
-answerList = []
 def solution(table):
 
-    answer = 0
+    answer = 1
 
     for indexTable in range(len(table)):
         if table[indexTable][1] not in hashTable.keys():
             hashTable.setdefault(table[indexTable][1], []).append(table[indexTable][0])
         else:
             hashTable.setdefault(table[indexTable][1], []).append(table[indexTable][0])
-
+    
     keys = hashTable.keys() # '가' '나' '다' '라' 
-    values = (hashTable[key] for key in keys)
+    values = [hashTable[key] for key in keys]
+    
+    ####only value
+    #for i in  range(len(values)):
+    #    answer *= len(values[i]) + 1
 
-    #values = [hashTable[key] for key in keys]  #[['1'],['2'],['3','4'],['5']]
-    #combinations = [dict(zip(keys, combination)) for combination in itertools.product(*values)]
-
-    #1. All combinations of Value of keys
+    ### value + string
     for i in  range(1, len(hashTable.values())+1):
-        #combinationsOfValues.append(list(itertools.combinations(values, i))) #[['1개 조합'],['2개 조합'],['3개 조합']- - -]
-        #print(list(itertools.combinations(values, i+1))[0])
+        for n in itertools.combinations(values,i): #[['n==1'],['n==2'],['n==3']- - -]
+            for element in itertools.product(*n):
+                print(element)
+                answer += 1
+             
+    #### once
+    #for i in  range(1, len(hashTable.values())+1):
+    #    listlist = (list(itertools.product(*n)) for n in itertools.combinations(list(values),i))
+    #    print(list(listlist))    
 
-        #answer += len(list(itertools.product(itertools.combinations(values,i))))
-        #print(list(itertools.product(itertools.combinations(values,i))))
-        #for n in itertools.combinations(values,i):
-            #print(n)
-            #print(list(itertools.product(*n)))
-            #answer += len(list(itertools.product(*n)))
-            #for element in itertools.product(*n):
-                #print(element)
-                 #answer += 1
-
-        print(list(values))
-        print(list(itertools.combinations(list(values),3)))
-
-        listlist = (len(list(itertools.product(*n))) for n in itertools.combinations(list(values),i))
-        answer += sum(list(listlist))    
-
-        #print(list(listlist))
-        #answer += (len(list(listlist)))
-    #print(list(combinationsOfValues[1]))
-
-    #2. product each combined list
-    #for i in range(len(combinationsOfValues)):
-    #    for j in range(len(list(combinationsOfValues[i]))):
-    #        for element in itertools.product(*list(combinationsOfValues[i][j])):
-    #            answerList.append(element)
+    answer -= 1
 
     print(answer)
 
