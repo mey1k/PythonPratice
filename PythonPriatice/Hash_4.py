13f6027a61e205f31bef143ce740bfc38ade2aa7
mjkim <mey12k@daum.net>
2019-04-06 15:07:47 +0900

    Hash 4 ing

--- PythonPriatice/Hash_4.py
+++ PythonPriatice/Hash_4.py
@@ -4,28 +4,28 @@
 
 '''
 
-HashOfHashTable = {}
+hashTable = {}
 def solution(genres, plays):
 
     answer = []
 
-    HashTable = {}
-
-    #for i in range(len(genres)):
-    #    HashTable.setdefault(genres[i], plays[i])
-    #    HashOfHashTable.setdefault(i, {genres[i]:plays[i]})
-
     for i in range(len(genres)):
-        HashOfHashTable.setdefault(plays[i], {genres[i]:i})
+        if genres[i] not in hashTable.keys():
+            hashTable.setdefault(genres[i], []).append(plays[i])
+        else:
+            hashTable.setdefault(genres[i], []).append(plays[i])
 
-    plays.sort(reverse=True)
-
-    answer = [HashOfHashTable.get(n) for n in plays]
-
-    
-
-    print(answer[0].keys())
+    while len(hashTable.keys()) != 0:
+        pivotGenres = max(hashTable, key = lambda x: hashTable.get(x))
+        pivotPlays = hashTable[pivotGenres]
+        pivotPlays.sort(reverse=True)
+        if(len(pivotPlays) > 1):
+            answer.append(plays.index(pivotPlays[0]))
+            answer.append(plays.index(pivotPlays[1]))
+        else:
+            answer.append(plays.index(pivotPlays[0]))
+        hashTable.pop(pivotGenres)
 
     return answer
 
-solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500])
+solution(["classic", "pop", "classic", "classic", "pop", "pop"], [500, 600, 150, 800, 2500, 250])
