855d92c7610e340181d6b0a07b161410e7cc5b1c
mey1k <mey1k@naver.com>
2019-04-19 10:17:37 +0900

    StackQueue_1 Complete

--- PythonPriatice/StackQueue_1.py
+++ PythonPriatice/StackQueue_1.py
@@ -1,1 +1,25 @@
+'''
 
+      arrangement	            return
+()(((()())(())()))(())	         17
+
+'''
+
+import queue
+
+def solution(arrangement):
+    answer = 0
+    Ironstick = queue.Queue()
+    arrangement = arrangement.replace('()', 'l') # l(((ll)(l)l))(l)
+    for pivot in arrangement:
+        if pivot == '(':
+            Ironstick.put('(')
+            answer += 1
+        elif pivot == ')':
+            Ironstick.get()
+        elif pivot == 'l':
+            answer += Ironstick.qsize()
+            
+    return answer
+
+solution("()(((()())(())()))(())")
