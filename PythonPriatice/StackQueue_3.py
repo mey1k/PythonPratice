6bf4bcf6bedadc24010e89b944d151e31a31b6f2
mey1k <mey1k@naver.com>
2019-04-23 18:10:20 +0900

    StackQueue_3 Complete

--- PythonPriatice/StackQueue_3.py
+++ PythonPriatice/StackQueue_3.py
@@ -1,30 +1,27 @@
 
 def solution(bridge_length, weight, truck_weights):
-    answer = 0
-    count = 0
+    shortestTime = 0
 
     passingTruck = [] 
     waitingTruck = []
-    passedTruck = []
 
     waitingTruck = truck_weights
+    passingTruck = [0]*bridge_length
 
-    while  passingTruck or waitingTruck:
-        answer += 1
-
-        if count >= bridge_length and bridge_length < answer:
-            passedTruck.append (passingTruck.pop(0))
-            count -= bridge_length
+    while  passingTruck:
+        shortestTime += 1
+        passingTruck.pop(0)
 
         if  waitingTruck:
             if(sum(passingTruck) +  waitingTruck[0] <= weight):
-               passingTruck.append(waitingTruck.pop(0))               
+               passingTruck.append(waitingTruck.pop(0))
+            else:
+               passingTruck.append(0)
 
-        count += len(passingTruck) 
+        print(shortestTime, '   ', passingTruck, '    ', waitingTruck)
 
-        print(answer,'      ',passedTruck,'     ', passingTruck,'    ', waitingTruck)
+    return print(shortestTime)
 
-    return print(answer)
 
 #solution(100, 100, [10])
 #solution(2, 10, [7, 4, 5, 6])
