629d0c8e9f7a6fd1e6fe666876d409b9a57f61ad
mey1k <mey1k@naver.com>
2019-05-07 16:37:47 +0900

    heap_2 complete

--- PythonPriatice/heap_2.py
+++ PythonPriatice/heap_2.py
@@ -2,17 +2,39 @@
 stock	    dates	        supplies	    k	    result
 4	            [4,10,15]   	[20,5,10]	    30	2
 '''
+#import heapq
+
+#def solution(stock, dates, supplies, k):
+#    answer = 0
+#    Day = 0
+#    i=0
+
+#    while stock < k-1:
+       
+#        if stock < dates[i] + (dates[i+1]-dates[i]):
+#            stock += supplies[i]
+#            answer += 1
+#            i += 1
+#        else:
+#            i += 1
+        
+
+#    return print(answer)
+
+#solution(4,[4,10,15],[20,5,10],30)
+import heapq
 
 def solution(stock, dates, supplies, k):
+
     answer = 0
-    limit = 0
-
-    for n in supplies:
-        limit += stock + n
-        answer +=1
-        if k-1 <= limit:
-            break
-
+    event = list(zip(dates, supplies)) # day, supply 합치기
+    heap = []
+    while stock < k: # 쌓여있는 자원이 안전할  때 까지 공급
+        while event and event[0][0] <= stock:  # 공급할 자원이 남아있고 그 자원의 공급일이 현재 자원 보다 작거나 같을경우 계속 공급
+            date, supply = event.pop(0)
+            heapq.heappush(heap, -supply) # 최대힙
+        stock += -heapq.heappop(heap)
+        answer += 1
     return print(answer)
 
 solution(4,[4,10,15],[20,5,10],30)
