af15053acecf51cf9dee341cb686810e05df3c74
mey1k <mey1k@naver.com>
2019-05-08 18:08:20 +0900

    heap_3 Complete

--- PythonPriatice/heap_3.py
+++ PythonPriatice/heap_3.py
@@ -5,28 +5,103 @@ jobs	                            return
 '''
 
 
-import heapq
+#import heapq
 
-def solution(jobs):
+#def solution(jobs):
 
-    answer = 0
+#    answer = 0
+#    heap = []
+#    workingTime = 0
+#    elapsedList = []
+#    sortedList = []
+
+#    for i in range(len(jobs)):
+#        heapq.heappush(heap, (jobs[i][1], jobs[i][0]))
+
+
+#    while heap:
+#        elapsedTime, startTime = heapq.heappop(heap)
+#        workingTime += elapsedTime
+#        elapsedSelf = workingTime - startTime
+#        if elapsedSelf > 0:
+#            elapsedList.append(elapsedSelf)
+
+#    answer = sum(elapsedList)//len(jobs)
+
+#    return print(answer)
+
+
+#solution([[0, 3], [1, 9], [2, 6], [4, 3]])
+
+from heapq import heappush, heappop
+import random
+
+def solution1(jobs):
+    jobs.sort(key=lambda x: x[0], reverse=True)
+    nums = len(jobs)
+    workingTime = 0
     heap = []
-    elapsedTime = 0
-    elapsedList = []
+    time = 0
 
-    for i in range(len(jobs)):
-        heapq.heappush(heap, jobs[i][1])
-        
-    heapq.heapify
+    while jobs:
+        while jobs and jobs[-1][0] <= time:
+            start, duration = jobs.pop()
+            heappush(heap, (duration, start))
+        if not heap:
+            start, duration = jobs.pop()
+            heappush(heap, (duration, start))
+            time = start
+        duration, start = heappop(heap)
+        print([duration,start])
+        time += duration
+        workingTime += time - start
+    
+    while heap:
+        duration, start = heappop(heap)
+        print([duration,start])
+        time += duration
+        workingTime += time - start
 
-    for j in range(len(heap)):
-        elapsedTime += heapq.heappop(heap)
-        elapsedSelf = elapsedTime - jobs[j][0]
-        elapsedList.append(elapsedSelf)
+    return print(workingTime//nums)
 
-    answer = sum(elapsedList)//len(jobs)
+def solution2(jobs):
+    jobs.sort(key=lambda x: x[0])
+    nums = len(jobs)
+    workingTime = 0
+    heap = []
+    time = 0
 
-    return print(answer)
+    while jobs:
+        while jobs and jobs[0][0] <= time:
+            start, duration = jobs.pop(0)
+            heappush(heap, (duration, start))
+        if not heap:
+            start, duration = jobs.pop(0)
+            heappush(heap, (duration, start))
+            time = start
+        duration, start = heappop(heap)
+        print([duration,start])
+        time += duration
+        workingTime += time - start
+    
+    while heap:
+        duration, start = heappop(heap)
+        print([duration,start])
+        time += duration
+        workingTime += time - start
 
+    return print(workingTime//nums)
 
-solution([[0, 3], [1, 9], [2, 6]])
+#jobs = []
+
+#for _ in range(10):
+#    jobs.append([random.randint(1,50), random.randint(1,50)])
+
+#jobs = [[24, 10], [18, 39], [34, 20], [37, 5], [47, 22], [20, 47], [15, 34], [15, 2], [35, 43], [26, 1]]
+jobs = [[24, 10], [18, 39], [34, 20], [37, 5], [47, 22], [20, 47], [15, 2], [15, 34], [35, 43], [26, 1]]
+#jobs = [[34,15],[2,15],[26,1]]
+
+print(jobs)
+
+solution1(jobs.copy())
+solution2(jobs.copy())
